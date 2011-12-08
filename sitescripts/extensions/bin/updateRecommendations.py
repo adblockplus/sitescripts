55cc75d7ac6d102e7a6a8f4eb18460cf817f6f66
Wladimir Palant <trev@adblockplus.org>
2011-12-08 16:28:25 +0100

    Recommended subscriptions: Automatically update the file in Adblock Plus for Chrome as well

--- sitescripts/extensions/bin/updateRecommendations.py
+++ sitescripts/extensions/bin/updateRecommendations.py
@@ -11,7 +11,8 @@ from sitescripts.subscriptions.bin.processTemplate import writeSubscriptions
 from tempfile import mkdtemp
 from shutil import rmtree
 
-def updateRecommendations(repository):
+def updateRecommendations():
+  repository = get_config().get('extensions', 'abp_repository')
   tempdir = mkdtemp(prefix='adblockplus')
   try:
     subprocess.Popen(['hg', 'clone',  '-U', repository, tempdir], stdout=subprocess.PIPE).communicate()
@@ -22,6 +23,17 @@ def updateRecommendations(repository):
   finally:
     rmtree(tempdir)
 
+  repository = get_config().get('extensions', 'abpchrome_repository')
+  tempdir = mkdtemp(prefix='adblockpluschrome')
+  try:
+    subprocess.Popen(['hg', 'clone',  '-U', repository, tempdir], stdout=subprocess.PIPE).communicate()
+    subprocess.Popen(['hg', 'up', '-R', tempdir, '-r', 'default'], stdout=subprocess.PIPE).communicate()
+    writeSubscriptions('recommendations', os.path.join(tempdir, 'subscriptions.xml'))
+    subprocess.Popen(['hg', 'commit', '-R', tempdir, '-u', 'hgbot', '-m', 'Updated list of recommended subscriptions'], stdout=subprocess.PIPE).communicate()
+    subprocess.Popen(['hg', 'push', '-R', tempdir], stdout=subprocess.PIPE).communicate()
+  finally:
+    rmtree(tempdir)
+
 if __name__ == '__main__':
   setupStderr()
-  updateRecommendations(get_config().get('extensions', 'abp_repository'))
+  updateRecommendations()
