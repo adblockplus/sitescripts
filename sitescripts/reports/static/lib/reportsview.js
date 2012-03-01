d586635a232969d4a488c5b5620cc194cedfee0c
Wladimir Palant <trev@adblockplus.org>
2012-03-01 08:10:26 +0100

    Issue report digests: Fixed: value to format is being passed in as a string

--- sitescripts/reports/static/lib/reportsview.js
+++ sitescripts/reports/static/lib/reportsview.js
@@ -45,7 +45,7 @@
   
   function NumberFormatter(row, cell, value, columnDef, dataContext)
   {
-    return value.toFixed(1);
+    return parseFloat(value).toFixed(1);
   }
 
   function TimeFormatter(row, cell, value, columnDef, dataContext)
