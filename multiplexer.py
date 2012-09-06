6ed63b6e7095ae1004cc8c38ced2744a78964046
Felix H. Dahlke <fhd@ubercode.de>
2012-09-06 19:07:24 +0200

    Use ' instead of "

--- multiplexer.py
+++ multiplexer.py
@@ -4,18 +4,18 @@ from urlparse import urlparse
 
 app = Flask(__name__)
 
-@app.route("/<path:path>")
+@app.route('/<path:path>')
 def multiplex(path):
   requestUrl = urlparse(request.url)
   print requestUrl.query
   requestPath = requestUrl.path
   if requestPath in handlers:
     # TODO: Some more environ entries are required for all scripts to work.
-    environ = {"QUERY_STRING": requestUrl.query}
+    environ = {'QUERY_STRING': requestUrl.query}
     # TODO: Actually return the supplied status/headers.
     start_response = lambda status, headers: None
     return handlers[requestPath](environ, start_response)
-  return ""
+  return ''
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   app.run(debug=True)
