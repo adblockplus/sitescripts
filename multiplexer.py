c77affe5f0d8a1850e24d50001465c7968bc3e25
Felix H. Dahlke <fhd@ubercode.de>
2012-09-07 09:35:26 +0200

    Use " instead of '

--- multiplexer.py
+++ multiplexer.py
@@ -4,17 +4,17 @@ from urlparse import urlparse
 
 app = Flask(__name__)
 
-@app.route('/<path:path>')
+@app.route("/<path:path>")
 def multiplex(path):
   request_url = urlparse(request.url)
   request_path = request_url.path
   if request_path in handlers:
     # TODO: Some more environ entries are required for all scripts to work.
-    environ = {'QUERY_STRING': request_url.query}
+    environ = {"QUERY_STRING": request_url.query}
     # TODO: Actually return the supplied status/headers.
     start_response = lambda status, headers: None
     return handlers[request_path](environ, start_response)
-  return ''
+  return ""
 
-if __name__ == '__main__':
+if __name__ == "__main__":
   app.run(debug=True)
--- sitescripts/crawler/web/crawler.py
+++ sitescripts/crawler/web/crawler.py
@@ -4,26 +4,26 @@ from sitescripts.web import url_handler
 
 @cached(600)
 def get_db():
-  database = get_config().get('crawler', 'database')
-  dbuser = get_config().get('crawler', 'dbuser')
-  dbpasswd = get_config().get('crawler', 'dbpassword')
-  if os.name == 'nt':
-    return MySQLdb.connect(user=dbuser, passwd=dbpasswd, db=database, use_unicode=True, charset='utf8', named_pipe=True)
+  database = get_config().get("crawler", "database")
+  dbuser = get_config().get("crawler", "dbuser")
+  dbpasswd = get_config().get("crawler", "dbpassword")
+  if os.name == "nt":
+    return MySQLdb.connect(user=dbuser, passwd=dbpasswd, db=database, use_unicode=True, charset="utf8", named_pipe=True)
   else:
-    return MySQLdb.connect(user=dbuser, passwd=dbpasswd, db=database, use_unicode=True, charset='utf8')
+    return MySQLdb.connect(user=dbuser, passwd=dbpasswd, db=database, use_unicode=True, charset="utf8")
 
 def execute_query(cursor, query, args=None):
   cursor.execute(query, args)
 
 def fetch_crawlable_urls():
   cursor = get_db().cursor(MySQLdb.cursors.DictCursor)
-  execute_query(cursor, 'SELECT url from crawler_urls')
+  execute_query(cursor, "SELECT url from crawler_urls")
   results = cursor.fetchall()
-  urls = [result['url'] for result in results]
+  urls = [result["url"] for result in results]
   return urls
 
-@url_handler('/crawlableUrls')
+@url_handler("/crawlableUrls")
 def crawlable_urls(environ, start_response):
   urls = fetch_crawlable_urls()
-  start_response('200 OK', [('Content-Type', 'text/plain')])
-  return '\n'.join(urls)
+  start_response("200 OK", [("Content-Type", "text/plain")])
+  return "\n".join(urls)
