0459535ef0f6ac80bb939193c8f41baaa9dea415
Andrey Novikov <andrey@adblockplus.org>
2012-10-18 12:52:08 +0400

    Updated reports schema to work without errors

--- sitescripts/reports/schema.sql
+++ sitescripts/reports/schema.sql
@@ -1,36 +1,5 @@
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `reports` (
-  `guid` varchar(40) NOT NULL,
-  `type` varchar(30) DEFAULT NULL,
-  `ctime` datetime NOT NULL,
-  `site` varchar(150) DEFAULT NULL,
-  `status` text,
-  `comment` text,
-  `contact` varchar(40) DEFAULT NULL,
-  `hasscreenshot` tinyint(1) NOT NULL DEFAULT '0',
-  `knownissues` int(10) unsigned NOT NULL DEFAULT '0',
-  `dump` mediumblob,
-  PRIMARY KEY (`guid`),
-  KEY `reports_ctime` (`ctime`)
-  KEY `reports_ibfk_1` (`contact`),
-  CONSTRAINT `reports_ibfk_1` FOREIGN KEY (`contact`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `sublists` (
-  `report` varchar(40) NOT NULL,
-  `list` int(10) unsigned NOT NULL,
-  `hasmatches` tinyint(1) NOT NULL DEFAULT '0',
-  UNIQUE KEY `report_list` (`list`,`report`),
-  KEY `sublists_ibfk_1` (`report`),
-  CONSTRAINT `sublists_ibfk_1` FOREIGN KEY (`report`) REFERENCES `reports` (`guid`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `sublists_ibfk_2` FOREIGN KEY (`list`) REFERENCES `subscriptions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `subscriptions` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `url` varchar(255) NOT NULL,
@@ -44,5 +13,36 @@ CREATE TABLE `users` (
   `reports` int(10) unsigned NOT NULL DEFAULT '0',
   `rating` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reports` (
+  `guid` varchar(40) NOT NULL,
+  `type` varchar(30) DEFAULT NULL,
+  `ctime` datetime NOT NULL,
+  `site` varchar(150) DEFAULT NULL,
+  `status` text,
+  `comment` text,
+  `contact` varchar(40) DEFAULT NULL,
+  `hasscreenshot` tinyint(1) NOT NULL DEFAULT '0',
+  `knownissues` int(10) unsigned NOT NULL DEFAULT '0',
+  `dump` mediumblob,
+  PRIMARY KEY (`guid`),
+  KEY `reports_ctime` (`ctime`),
+  KEY `reports_contract` (`contact`),
+  CONSTRAINT `reports_contract` FOREIGN KEY (`contact`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sublists` (
+  `report` varchar(40) NOT NULL,
+  `list` int(10) unsigned NOT NULL,
+  `hasmatches` tinyint(1) NOT NULL DEFAULT '0',
+  UNIQUE KEY `report_list` (`list`,`report`),
+  KEY `sublists_report` (`report`),
+  CONSTRAINT `sublists_report` FOREIGN KEY (`report`) REFERENCES `reports` (`guid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `sublists_list` FOREIGN KEY (`list`) REFERENCES `subscriptions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
