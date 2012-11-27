a86c4e3daf8e7c9320c54885367b256a87b8a7ee
Wladimir Palant <trev@adblockplus.org>
2012-11-27 16:21:15 +0100

    HG management: Allow hg mirrors to access all repositories over SSH

--- sitescripts/management/bin/generateHgAuth.py
+++ sitescripts/management/bin/generateHgAuth.py
@@ -41,6 +41,7 @@ def generateData(authRepo):
         'keytype': 'rsa',
         'disabled': False,
         'trusted': False,
+        'mirror': False,
         'repos': []
       }
       for option in options:
@@ -50,6 +51,8 @@ def generateData(authRepo):
           user['disabled'] = True
         elif option == 'trusted':
           user['trusted'] = True
+        elif option == 'mirror':
+          user['mirror'] = True
         else:
           print >>sys.stderr, 'Unknown user option: %s' % option
       user['key'] = re.sub(r'\s', '', tarFile.extractfile(fileInfo).read())
@@ -77,7 +80,7 @@ def generateData(authRepo):
       continue
     yield 'no-pty,environment="HGUSER=%s",environment="HGREPOS=%s" %s %s\n' % (
       user['name'] if not user['trusted'] else '',
-      ' '.join(user['repos']),
+      ' '.join(user['repos']) if not user['mirror'] else '*',
       'ssh-rsa' if user['keytype'] == 'rsa' else 'ssh-dss',
       user['key']
     )
