5bbf6d9e03b2ec88880c201f58be42f81f0a1051
Felix Dahlke <felix@adblockplus.org>
2013-11-16 16:44:48 +0100

    Add missing import

--- sitescripts/management/bin/start_services.py
+++ sitescripts/management/bin/start_services.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Adblock Plus.  If not, see <http://www.gnu.org/licenses/>.
 
-import os, subprocess
+import exceptions, os, subprocess
 from sitescripts.utils import get_config
 
 def _get_services():
