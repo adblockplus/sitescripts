b2627beb0dbeb962001f2f1552e70b76a983a716
Vasily Kuznetsov <vasily@adblockplus.org>
2016-09-13 10:52:29 +0200

    Fixes 4377 - Add Configurable Form to Email Service
    This work was originally started by Jon
    (see https://codereview.adblockplus.org/29352643/).

---
+++ sitescripts/formmail/test/test_formmail2.py
@@ -0,0 +1,110 @@
+# This file is part of the Adblock Plus web scripts,
+# Copyright (C) 2006-2016 Eyeo GmbH
+#
+# Adblock Plus is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# Adblock Plus is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Adblock Plus.  If not, see <http://www.gnu.org/licenses/>.
+from urllib import urlencode
+from urllib2 import urlopen, HTTPError
+
+import pytest
+from wsgi_intercept import (urllib_intercept, add_wsgi_intercept,
+                            remove_wsgi_intercept)
+
+from sitescripts.formmail.web import formmail2
+
+
+@pytest.fixture()
+def form_config():
+    return formmail2.conf_parse(formmail2.get_config_items())['test']
+
+
+@pytest.fixture()
+def form_handler(form_config):
+    return formmail2.make_handler('test', form_config)[1]
+
+
+# We make this a fixture instead of a constant so we can modify it in each
+# test as needed without affecting other tests.
+@pytest.fixture
+def form_data():
+    return {
+        'email': 'john_doe@gmail.com',
+        'mandatory': 'john_doe@gmail.com',
+        'non_mandatory_message': 'Once upon a time\nthere lived a king.',
+        'non_mandatory_email': 'test@test.com'
+    }
+
+
+@pytest.fixture()
+def response_for(form_handler):
+    host, port = 'test.local', 80
+    urllib_intercept.install_opener()
+    add_wsgi_intercept(host, port, lambda: form_handler)
+    url = 'http://{}:{}'.format(host, port)
+
+    def response_for(data):
+        if data is None:
+            response = urlopen(url)
+        else:
+            response = urlopen(url, urlencode(data))
+        return response.code, response.read()
+
+    yield response_for
+    remove_wsgi_intercept()
+
+
+def test_config_parse(form_config):
+    assert form_config['url'].value == 'test/apply/submit'
+    assert form_config['fields']['email'].value == 'mandatory, email'
+
+
+def test_success(response_for, form_data, mocker):
+    sm_mock = mocker.patch('sitescripts.formmail.web.formmail2.sendMail')
+    assert response_for(form_data) == (200, '')
+    assert sm_mock.call_count == 1
+    params = sm_mock.call_args[0][1]['fields']
+    assert set(params.keys()) == set(form_data.keys())
+    for key, value in form_data.items():
+        assert params[key] == value
+
+
+def test_non_mandatory_no_msg(response_for, form_data, mocker):
+    mocker.patch('sitescripts.formmail.web.formmail2.sendMail')
+    form_data['non_mandatory'] = ''
+    assert response_for(form_data) == (200, '')
+
+
+def test_invalid_email_cstm_msg(response_for, form_data, mocker, form_config):
+    mocker.patch('sitescripts.formmail.web.formmail2.sendMail')
+    form_data['email'] = 'bademail'
+    with pytest.raises(HTTPError) as error:
+        response_for(form_data)
+    assert error.value.read() == 'You failed the email validation'
+
+
+def test_valid_nan_mandatory_email(response_for, form_data, mocker):
+    mocker.patch('sitescripts.formmail.web.formmail2.sendMail')
+    form_data['non_mandatory_email'] = 'asfaf'
+    with pytest.raises(HTTPError) as error:
+        response_for(form_data)
+    assert error.value.read() == 'Invalid email'
+
+    del form_data['non_mandatory_email']
+    assert response_for(form_data) == (200, '')
+
+
+def test_mandatory_fail_dflt_msg(response_for, form_data, mocker):
+    mocker.patch('sitescripts.formmail.web.formmail2.sendMail')
+    del form_data['mandatory']
+    with pytest.raises(HTTPError) as error:
+        response_for(form_data)
+    assert error.value.read() == 'No mandatory entered'
---
+++ sitescripts/formmail/web/formmail2.py
@@ -0,0 +1,105 @@
+# This file is part of the Adblock Plus web scripts,
+# Copyright (C) 2006-2016 Eyeo GmbH
+#
+# Adblock Plus is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# Adblock Plus is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Adblock Plus.  If not, see <http://www.gnu.org/licenses/>.
+
+import datetime
+import collections
+
+from sitescripts.utils import get_config, sendMail, encode_email_address
+from sitescripts.web import registerUrlHandler, form_handler
+
+
+def get_config_items():
+    config = get_config()
+    default_keys = set(config.defaults())
+    for name, value in config.items('formmail2'):
+        if name not in default_keys:
+            yield name, value
+
+
+def store_value(conf_dict, path, value):
+    head, tail = path[0], path[1:]
+    if head not in conf_dict:
+        conf_dict[head] = collections.OrderedDict()
+    if tail:
+        store_value(conf_dict[head], tail, value)
+    else:
+        conf_dict[head].value = value
+
+
+def conf_parse(conf_items):
+    conf_dict = collections.OrderedDict()
+    for key, value in conf_items:
+        path = key.split('.')
+        store_value(conf_dict, path, value)
+    return conf_dict
+
+
+def make_error(spec, check_type, default_message):
+    if check_type in spec:
+        return spec[check_type].value
+    return default_message
+
+
+def make_handler(name, config):
+    try:
+        url = config['url'].value
+    except (KeyError, AttributeError):
+        raise Exception('No URL configured for form handler:' + name)
+    try:
+        template = config['template'].value
+    except (KeyError, AttributeError):
+        raise Exception('No template configured for form handler:' + name)
+    try:
+        fields = config['fields']
+        for field, spec in fields.items():
+            spec.value = {s.strip() for s in spec.value.split(',')}
+    except KeyError:
+        raise Exception('No fields configured for form handler:' + name)
+    if len(fields) == 0:
+        raise Exception('No fields configured for form handler:' + name)
+
+    @form_handler
+    def handler(environ, start_response, params):
+        response_headers = [('Content-Type', 'text/plain; charset=utf-8')]
+        errors = []
+        for field, spec in fields.items():
+            if 'mandatory' in spec.value:
+                if field not in params.keys():
+                    errors.append(make_error(spec, 'mandatory',
+                                             'No {} entered'.format(field)))
+            if 'email' in spec.value and field in params.keys():
+                try:
+                    params[field] = encode_email_address(params[field])
+                except ValueError:
+                    errors.append(make_error(spec, 'email', 'Invalid email'))
+        if errors:
+            start_response('400 Bad Request', response_headers)
+            return '\n'.join(errors)
+
+        template_args = {
+            'time': datetime.datetime.now(),
+            'fields': {field: params.get(field, '') for field in fields}
+        }
+        sendMail(template, template_args)
+        start_response('200 OK', response_headers)
+        return ''
+
+    return url, handler
+
+
+conf_dict = conf_parse(get_config_items())
+for name, config in conf_dict.items():
+    url, handler = make_handler(name, config)
+    registerUrlHandler(url, handler)
