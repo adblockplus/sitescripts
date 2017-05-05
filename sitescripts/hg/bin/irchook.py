f49cc0a6098d9668cc49c48d6e951ba7dadf83a3
Wladimir Palant <trev@adblockplus.org>
2017-05-05 10:44:28 +0200

    Issue 5212 - IRC hook should report bookmarked branches

--- sitescripts/hg/bin/irchook.py
+++ sitescripts/hg/bin/irchook.py
@@ -7,9 +7,12 @@ from sitescripts.utils import get_config
 
 def hook(ui, repo, node=None, **kwargs):
     ctx = repo[node]
+    commit_identifiers = ctx.bookmarks()
+    if not commit_identifiers or ctx.branch() != 'default':
+        commit_identifiers.append(ctx.branch())
     remote = [get_config().get('irchook', 'remote_command'),
-              os.path.basename(repo.root), str(ctx.branch()), str(ctx.user()),
-              str(ctx), str(ctx.description())]
+              os.path.basename(repo.root), ','.join(commit_identifiers),
+              str(ctx.user()), str(ctx), str(ctx.description())]
     remote = ' '.join(map(lambda s: pipes.quote(s), remote))
 
     command = ['ssh', get_config().get('irchook', 'remote_host'), remote]
